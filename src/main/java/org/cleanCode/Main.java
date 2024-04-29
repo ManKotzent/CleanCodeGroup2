2eccb9e038607f867fa9a8b98abd1fd869c1fa2a
filipped <82341252+FilippEder@users.noreply.github.com>
2024-04-29 05:04:48 +0200

    Refactored TranslatorApi Class. Created TranslatorApi Tests

--- src/main/java/org/cleanCode/Main.java
+++ src/main/java/org/cleanCode/Main.java
@@ -1,7 +1,17 @@
 package org.cleanCode;
 
+import org.cleanCode.CrawlerRecord.CrawlerRecord;
+import org.cleanCode.CrawlerRecord.CrawlerRecordFactory;
+import org.cleanCode.markdownFileCreator.MarkdownFileCreator;
+
 public class Main {
+    private static final String url ="https://stackoverflow.com/";
+    private static final int depth = 1;
+    private static final String[] domains = {""};
+
     public static void main(String[] args) {
+        CrawlerRecord record = CrawlerRecordFactory.generateCrawlerRecord(url,depth);
+        MarkdownFileCreator.createMdFile(record);
 
     }
 }
--- src/main/java/org/cleanCode/markdownFileCreator/MarkdownFileCreator.java
+++ src/main/java/org/cleanCode/markdownFileCreator/MarkdownFileCreator.java
@@ -1,23 +1,38 @@
 package org.cleanCode.markdownFileCreator;
 
+import org.cleanCode.CrawlerRecord.CrawlerRecord;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MarkdownFileCreator {
-    String filePath = "example.md";
 
 
-    public void createMdFile(String data){
+    public static void createMdFile(CrawlerRecord record){
         try{
+            String filePath = "example.md";
             FileWriter fileWriter = new FileWriter(filePath);
             PrintWriter printWriter = new PrintWriter(fileWriter);
 
-            printWriter.print(data);
+
+            buildReport(record,fileWriter);
 
             printWriter.close();
             fileWriter.close();
         }catch (IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void buildReport(CrawlerRecord record, FileWriter writer) throws IOException {
+        try{
+            writer.write("input: <a><a>\n");
+            writer.write("<br>depth: "+record.getURL()+"\n");
+            writer.write("<br>source language:\n");
+            writer.write("<br>target language:\n");
+            writer.write("<br>summary\n");
+        } catch (IOException e){
             System.err.println(e.getMessage());
         }
     }
